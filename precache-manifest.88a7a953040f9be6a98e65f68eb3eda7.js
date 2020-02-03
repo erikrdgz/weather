d230cb5594de4c622d7786671770a8ad92895d37
Erik <erikrdgz@hotmail.com>
2020-02-03 13:46:53 -0700

    Updates

---
+++ precache-manifest.88a7a953040f9be6a98e65f68eb3eda7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f6bc4a0b022d50a1dc46534a7d493df",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "3b26f94350cfcaf1c0b7",
+    "url": "/weather/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "95b98a1bbe772edc1eb5",
+    "url": "/weather/static/css/main.0d6bc54b.chunk.css"
+  },
+  {
+    "revision": "3b26f94350cfcaf1c0b7",
+    "url": "/weather/static/js/2.db98ef1d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/weather/static/js/2.db98ef1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95b98a1bbe772edc1eb5",
+    "url": "/weather/static/js/main.7961f640.chunk.js"
+  },
+  {
+    "revision": "c99e76bcf2bdae1a8b8a",
+    "url": "/weather/static/js/runtime-main.953fba7f.js"
+  },
+  {
+    "revision": "09c61e73628172da9557daf7371f2696",
+    "url": "/weather/static/media/background.09c61e73.jpg"
+  }
+]);
